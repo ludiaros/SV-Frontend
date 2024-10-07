a76b7ada0175a081933357eaa7d668010384b548
casariz <danielfelipedfco@gmail.com>
2024-10-07 09:16:02 -0500

    Agregar funcionalidad página de rutas

--- src/app/pages/route/route.page.ts
+++ src/app/pages/route/route.page.ts
@@ -27,6 +27,8 @@ export class RoutePage implements OnInit {
 
   async loadRoutes() {
     this.routes = await this.api.getRoute();
+    console.log(this.routes);
+    
     this.filteredRoutes = this.routes;
   }
 

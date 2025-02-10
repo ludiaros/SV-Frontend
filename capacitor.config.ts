2f7cbb017c17410cb63a3a63de850dfb04929daa
casariz <danielfelipedfco@gmail.com>
2025-02-10 11:29:47 -0500

    Cambiar nombre app: sg-rosal => SVRosal

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'sg-rosal',
+  appName: 'SVRosal',
   webDir: 'www'
 };
 
